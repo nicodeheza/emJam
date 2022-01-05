bbcd3a3d4c420b8a27266288b876aafb67ae1abf
nicodeheza <nicolasdeheza@hotmail.com>
2022-01-05 13:53:48 -0300

    node-app proxy

--- client/src/redux/actions/chatActions.js
+++ client/src/redux/actions/chatActions.js
@@ -9,7 +9,7 @@ import {
 } from "../consts";
 
 export const connect = () => (dispatch, getState) => {
-	const ws = new WebSocket("ws://emjam.nicolasdeheza.com:8080");
+	const ws = new WebSocket("ws://emjam.nicolasdeheza.com/ws");
 
 	dispatch({
 		type: CONNECT,
